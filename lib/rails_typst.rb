c5bf5cb0fe48596e78d4590d96d415ba20b7a6ff
Sean Harmer <sean@theharmers.co.uk>
2024-10-20 14:17:42 +0100

    Initial commit

---
+++ lib/rails_typst.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_typst/version"
+
+module RailsTypst
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_typst/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsTypst
+  VERSION = "0.1.0"
+end
